be575bb4d29a07c3d641816ade8adaea54f038a7
Katsushi21 <sheoaspendie@gmail.com>
2020-11-25 16:23:30 +0900

    me-ruform

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,42 @@
+from .settings_common import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DEFAULT_LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(Lineno)d)',
+                '%(message)s',
+                ])
+        }
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'formatter': 'dev',
+            'class': 'logging.StreamHandler',
+        }
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
