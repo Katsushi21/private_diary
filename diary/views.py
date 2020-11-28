b1f4c5c1d362024c29c3ab6084444257775a9424
Katsushi21 <sheoaspendie@gmail.com>
2020-11-28 21:40:03 +0900

    p.280

--- diary/views.py
+++ diary/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Diary
 
+from .forms import InquiryForm, DiaryCreateForm
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -36,3 +39,59 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    slug_field = "title"
+    slug_url_kwarg = "title"
+
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_valid(form)
+
+
+class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Diary
+    template_name = 'diary_update.html'
+    form_class = DiaryCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('diary:diary_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request, '日記を更新しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, '日記の更新に失敗しました。')
+        return super().form_invalid(form)
+
+
+class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = 'Diary'
+    template_name = 'diary_delete.html'
+    success_url = reverse_lazy('diary:diary_list')
+
+    def __delete__(self, request, *args, **kwargs):
+        messages.success(self.request, "日記を削除しました。")
+        return super().delete(request, *args, **kwargs)
